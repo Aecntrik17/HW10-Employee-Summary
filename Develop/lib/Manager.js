d40271b370461edadab2ed182f85296c8a7ed2f6
Aaron Sims <aaron@adsimsllc.com>
2020-07-24 19:50:05 -0400

    building class for Manager in manager.js file

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,19 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// TODO: Write code to define and export the Manager class.
+// pulling data from the Employee class file
+const Employee = require("./employee");
+
+class Manager extends Employee {
+  constructor(name, id, email, officeNumber) {
+    super(name, id, email);
+    this.officeNumber = officeNumber;
+  }
+
+  getRole() {
+    return "Manager";
+  }
+  getOfficeNumber() {
+    return this.officeNumber;
+  }
+}
+
+module.exports = Manager;
