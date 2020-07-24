003475cae1a07bca4ea8574b0d734351b6cb26d1
Aaron Sims <aaron@adsimsllc.com>
2020-07-24 19:34:48 -0400

    building out employee.js file as initial class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    //   setting necesary methods
+  }
+  getName() {
+    return this.name;
+  }
+  getId() {
+    return this.id;
+  }
+  getEmail() {
+    return this.email;
+  }
+  getRole() {
+    return "Employee";
+  }
+}
+
+//   making sure that other files can pull the information from this file
+module.exports = Employee;
