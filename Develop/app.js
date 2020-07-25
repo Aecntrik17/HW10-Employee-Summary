2192720a9211176b2b53485996ca604969fe1cc7
Aaron Sims <aaron@adsimsllc.com>
2020-07-24 20:23:02 -0400

    creating employeeArray which holds objects generated by the answers of the user inputs

--- Develop/app.js
+++ Develop/app.js
@@ -108,6 +108,8 @@ async function addEmployee() {
     addEmployee();
   }
 }
+// this array will hold the objects of employees that are generated
+const employeeArray = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
